20691af19fcdfdaa81d1eb20e0b0d7d9f657a873
mokong03 <mark.limon03@gmail.com>
2021-04-15 12:43:45 -0700

    1st commit

---
+++ ZybooksHW1/CH7-2-3Array/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+    public class Main {
+
+        public static void main(String[] args) {
+            // write your code here
+
+            int [] courseGrades = {7, 9, 11, 10};
+
+            for (int i = 0; i < courseGrades.length; ++i) {
+                System.out.print( courseGrades[i] + " ");
+
+            }
+            System.out.println();
+            int elementZero = courseGrades[0];
+            int elementOne = courseGrades[1];
+            for (int y = 0; y < courseGrades.length-2; ++y) {
+                courseGrades[y] = courseGrades[courseGrades.length - 1- y];
+                System.out.print(courseGrades[y] + " ");
+
+            }
+            System.out.println(elementOne + " "+ elementZero + " ");
+
+
+        }
+    }
+
